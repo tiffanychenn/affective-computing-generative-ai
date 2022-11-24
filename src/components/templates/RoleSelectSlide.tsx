d3327a6cbefafbf3c99d5d7f02a5af611a613e59
Tiffany Chen <tiffanysta.c@gmail.com>
2022-11-24 16:43:27 -0500

    quick fix for landscape vs buildings hahhahahhahahhahhaa

--- src/components/templates/RoleSelectSlide.tsx
+++ src/components/templates/RoleSelectSlide.tsx
@@ -21,7 +21,7 @@ interface State {
 	landscapePlayer: 1 | 2 | undefined;
 }
 
-const ROLE_SELECT_QUESTION_ID = "role-select-landscape";
+const ROLE_SELECT_QUESTION_ID = "role-select";
 const ROLE_SELECT_VALUE_P1 = "Player 1";
 const ROLE_SELECT_VALUE_P2 = "Player 2";
 
@@ -41,7 +41,7 @@ export class RoleSelectSlide extends React.Component<Props, State> {
 			backgroundImage: STAR_BG,
 			player: 'both',
 			playerAction: 'Discuss the following',
-			requiredFormElemIds: ["world-tags", ROLE_SELECT_QUESTION_ID],
+			requiredFormElemIds: ["world-tags"],
 			maxWidthIfNoImageCard: false,
 			itemSpacing: GAP_BETWEEN_QUESTIONS,
 			makeContent: (logger, hasTimedOut, renderText) => {
@@ -56,7 +56,7 @@ export class RoleSelectSlide extends React.Component<Props, State> {
 					</QuestionGroup>
 					<QuestionGroup>
 						<PageHeader>Between the two of you... who is closest to nature?</PageHeader>
-						<RadioGroup id={ROLE_SELECT_QUESTION_ID}
+						<RadioGroup id={ROLE_SELECT_QUESTION_ID + "-landscape"}
 									logger={logger}
 									ids={["role-select-landscape-p1", "role-select-landscape-p2"]}
 									labels={[ROLE_SELECT_VALUE_P1, ROLE_SELECT_VALUE_P2]}
@@ -64,10 +64,10 @@ export class RoleSelectSlide extends React.Component<Props, State> {
 									onSelect={value => this.setState({ landscapePlayer: value == ROLE_SELECT_VALUE_P1 ? 1 : 2 })}
 									selected={this.state.landscapePlayer === 1 ? ROLE_SELECT_VALUE_P1 : (this.state.landscapePlayer === 2 ? ROLE_SELECT_VALUE_P2 : undefined)}/>
 					<PageHeader>and who most enjoys to build?</PageHeader>
-					<RadioGroup id="role-select-nature"
+					<RadioGroup id={ROLE_SELECT_QUESTION_ID + "-buildings"}
 								logger={logger}
-								ids={["role-select-landscape-p1", "role-select-landscape-p2"]}
-								labels={["Player 1", "Player 2"]}
+								ids={["role-select-building-p1", "role-select-building-p2"]}
+								labels={[ROLE_SELECT_VALUE_P1, ROLE_SELECT_VALUE_P2]}
 								orientation="horizontal"
 								onSelect={value => this.setState({ landscapePlayer: value == ROLE_SELECT_VALUE_P1 ? 2 : 1 })}
 								selected={this.state.landscapePlayer === 1 ? ROLE_SELECT_VALUE_P2 : (this.state.landscapePlayer === 2 ? ROLE_SELECT_VALUE_P1 : undefined) }/>
